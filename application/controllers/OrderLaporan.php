956398619c7c4de7f2ef296bff1f5b39bfe7f9f4
Ion Zero <karyonosukses@gmail.com>
2022-05-09 16:06:25 +0700

    update

---
+++ application/controllers/OrderLaporan.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class OrderLaporan extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+		$this->load->model('M_order', 'record');
+		$this->load->model('M_auth', 'Auth');
+		$this->Auth->cek_login();
+	}
+
+	public function index()
+	{
+		$data['title'] = 'Dashboard | Marak';
+		$this->load->view('Order/v_orderlaporan', $data);
+	}
+}
---
+++ application/views/Order/v_index.php
@@ -0,0 +1,56 @@
+<?php $this->load->view('Components/v_header'); ?>
+
+<!-- BEGIN: Body-->
+<?php $this->load->view('Components/v_headerbottom'); ?>
+
+<!-- BEGIN: Header-->
+<?php $this->load->view('Components/v_navbar'); ?>
+<!-- END: Header-->
+
+
+<!-- BEGIN: Main Menu-->
+<?php $this->load->view('Components/v_sidebar'); ?>
+<!-- END: Main Menu-->
+
+<!-- BEGIN: Content-->
+<div class="app-content content ">
+	<div class="content-overlay"></div>
+	<div class="header-navbar-shadow"></div>
+	<div class="content-wrapper container-xxl p-0">
+		<div class="content-header row">
+			<div class="content-header-left col-md-9 col-12 mb-2">
+				<div class="row breadcrumbs-top">
+					<div class="col-12">
+						<h2 class="content-header-title float-start mb-0">Dashboard</h2>
+						<div class="breadcrumb-wrapper">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="<?= base_url('Order'); ?>">Admin Marketing</a>
+								</li>
+								<li class="breadcrumb-item active">Dashboard
+								</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="content-header-right text-md-end col-md-3 col-12 d-md-block d-none">
+				<div class="mb-1 breadcrumb-right">
+					<div class="dropdown">
+						<button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="grid"></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content-body">
+			<!-- Dashboard Ecommerce Starts -->
+			<p>content</p>
+			<!-- Dashboard Ecommerce ends -->
+
+		</div>
+	</div>
+</div>
+<!-- END: Content-->
+
+<?php $this->load->view('Components/v_footer'); ?>
+<?php $this->load->view('Components/v_bottom'); ?>
