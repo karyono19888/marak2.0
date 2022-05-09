dd44bf9b9b0fc5f2c800522cfbad2a3ff595b888
Ion Zero <karyonosukses@gmail.com>
2022-05-09 13:47:18 +0700

    feed

--- application/views/Profile/v_profile.php
+++ application/views/Profile/v_profile.php
@@ -175,14 +175,4 @@
 		});
 	});
 </script>
-<script>
-	$(window).on('load', function() {
-		if (feather) {
-			feather.replace({
-				width: 14,
-				height: 14
-			});
-		}
-	})
-</script>
 <?php $this->load->view('Components/v_bottom'); ?>
